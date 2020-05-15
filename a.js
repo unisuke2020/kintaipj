033c9dea1a7dec992a6183136de97178824f958d
unisuke2020 <64544085+unisuke2020@users.noreply.github.com>
2020-05-15 17:58:20 +0900

    aa

---
+++ a.js
@@ -0,0 +1,23 @@
+$(function(){
+	setInterval(function(){
+		var now = new Date();
+		var y = now.getFullYear();
+		var m = now.getMonth() + 1;
+		var d = now.getDate();
+		var w = now.getDay();
+		var wd = ['日', '月', '火', '水', '木', '金', '土'];
+		var h = now.getHours();
+		var mi = now.getMinutes();
+		var s = now.getSeconds();
+		var mm = ('0' + m).slice(-2);
+		var dd = ('0' + d).slice(-2);
+		var hh = ('0' + h).slice(-2);
+		var mmi = ('0' + mi).slice(-2);
+		var ss = ('0' + s).slice(-2);
+	$('#hizuke').text(y + '年' + mm + '月' + dd + '日' + '(' + wd[w] + ')');
+	$('#tbfs').css("font-size","200%");
+	$('#hours').text(hh);
+	$('#minutes').text(mmi);
+	$('#flash').fadeOut(1000).fadeIn(1000);
+	}, 1000);
+});
