76c8fdfc27a50912a2f897fbbdfa57f183ccd596
unisuke2020 <64544085+unisuke2020@users.noreply.github.com>
2020-05-21 11:29:00 +0900

    Update a.js

--- a.js
+++ a.js
@@ -2,13 +2,13 @@ $(function() {
   var h = $(window).height();
   
   $('#wrap').css('display','none');
-  $('#loader-bg ,#loader').height(h).css('display','black');
+  $('#loader-bg ,#loader').height(h).css('display','none');
 });
   
 $(window).load(function () { //全ての読み込みが完了したら実行
   $('#loader-bg').delay(900).fadeOut(800);
   $('#loader').delay(600).fadeOut(300);
-  $('#wrap').css('display', 'black');
+  $('#wrap').css('display', 'none');
 });
   
 //10秒たったら強制的にロード画面を非表示
