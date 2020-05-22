55588f227a2b51e3762b5c8c8b01194da911c551
unisuke2020 <64544085+unisuke2020@users.noreply.github.com>
2020-05-22 19:15:08 +0900

    Update a.js

--- a.js
+++ a.js
@@ -80,7 +80,8 @@ $(function(){
 	   if (navigator.geolocation) {
       		navigator.geolocation.getCurrentPosition(
 		function(position){
-        	        $('#coment').text(position.coords);
+        	       // $('#coment').text(position.coords);
+			 $('#coment').text(error.code);
 	      });
            } else { // 現在位置を取得できない場合の処理
             $('#coment').text("ご使用中のブラウザは現在地検索に対応されておりません。");
