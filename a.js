8b1e2d87801c2ccf9ed83f2ac8b55c759c3cd022
unisuke2020 <64544085+unisuke2020@users.noreply.github.com>
2020-05-23 18:44:28 +0900

    Update a.js

--- a.js
+++ a.js
@@ -79,8 +79,9 @@ $(function(){
 	$('.btn-circle-border-simple').on('click', function() {
 	   if (navigator.geolocation) {
       		navigator.geolocation.getCurrentPosition(function(position){
-		//$('#coment').text(position.coords.latitude);
-		alert("緯度:"+position.coords.latitude+",経度"+position.coords.longitude);
+		$('#coment').text(position.coords.latitude+","+position.coords.longitude);
+		getLocationName(position.coords.latitude,position.coords.longitude);
+		$('#coment2').text(locationName);
 	      });
            } else { // 現在位置を取得できない場合の処理
             $('#coment').text("ご使用中のブラウザは現在地検索に対応されておりません。");
